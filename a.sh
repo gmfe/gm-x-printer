d339d9af6831aa0a8cee5f14fe0be4731973031a
baozouai <baozouai@gmail.com>
2023-05-09 12:03:18 +0800

    chore: publish判断是否beta,alpha

--- a.sh
+++
@@ -1,7 +0,0 @@
-if [[ refs/tags/v1.8.7-beta.6 == *refs/tags/*-beta* ]]; then
-    echo "1"
-  elif [[ refs/tags/v1.8.7-beta.6 == *refs/tags/*-alpha* ]]; then
-    echo "2"
-  else
-    echo "3"
-  fi
