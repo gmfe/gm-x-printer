edda7e894d626fdb9414d49331b08ebf7c028994
Yu <yu.gintoki@gmail.com>
2025-06-09 14:57:13 +0800

    Feature/2505 s1 lite (#105)
    * feat: 【1041650】分拣二维表（按商品分拣、按客户分拣）
    
    * feat: 【1041918】报价单打印和分享不展示价格为0和已下架的商品
    
    * chore: release v1.11.4-beta.0
    
    * fix: 【1041918】报价单打印和分享不展示价格为0和已下架的商品
    
    * chore: release v1.11.4-beta.1
    
    * chore: release v1.11.4-beta.2
    
    * fix: 分拣单 bug
    
    * fix: 分拣单 bug
    
    * chore: release v1.11.4-beta.3
    
    * chore: release v1.13.0-beta.0

---
+++ src/editor_sorting/context_menu.js
@@ -0,0 +1,81 @@
+import i18next from '../../locales'
+import React from 'react'
+import CommonContextMenu from '../common/common_context_menu'
+import { inject, observer } from 'mobx-react'
+import { Printer } from '../printer'
+import _ from 'lodash'
+import { BLOCK_TYPE_LIST } from './constants'
+
+@inject(stores => ({
+  editStore: stores.editStore,
+  mockData: stores.mockData
+}))
+@observer
+class ContextMenu extends React.Component {
+  hasSubtotalBtn = name => {
+    if (!name) return false
+    const noSubtotalList = ['abnormal', 'abnormalDetails']
+    const arr = name.split('.')
+    if (_.includes(arr, 'table')) {
+      const dataKey = this.props.editStore.config.contents[arr[2]].dataKey
+      return !_.includes(noSubtotalList, dataKey)
+    }
+  }
+
+  handleChangeTableDataKey = (key, name) => {
+    const { editStore } = this.props
+
+    editStore.changeTableDataKey(name, key)
+  }
+
+  renderOrderActionBtn = name => {
+    if (!this.hasSubtotalBtn(name)) {
+      return null
+    }
+
+    const arr = name.split('.')
+    const { dataKey } = this.props.editStore.config.contents[arr[2]]
+    const keyArr = dataKey.split('_')
+
+    const isMultiActive = keyArr.includes('multi')
+    const isCategoryActive = keyArr.includes('category')
+
+    return (
+      <>
+        {/* <div */}
+        {/*   onClick={this.handleChangeTableDataKey.bind(this, 'multi', name)} */}
+        {/*   className={isMultiActive ? 'active' : ''} */}
+        {/* > */}
+        {/*   {i18next.t('双栏商品')} */}
+        {/* </div> */}
+        {/* <div */}
+        {/*   onClick={this.handleChangeTableDataKey.bind(this, 'category', name)} */}
+        {/*   className={isCategoryActive ? 'active' : ''} */}
+        {/* > */}
+        {/*   {i18next.t('商品分类')} */}
+        {/* </div> */}
+      </>
+    )
+  }
+
+  render() {
+    const { editStore, mockData, uploadQiniuImage } = this.props
+    return (
+      <CommonContextMenu
+        // renderTableAction={this.renderOrderActionBtn}
+        insertBlockList={BLOCK_TYPE_LIST}
+        uploadQiniuImage={uploadQiniuImage}
+      >
+        <Printer
+          key={editStore.computedPrinterKey}
+          selected={editStore.selected}
+          selectedRegion={editStore.selectedRegion}
+          config={editStore.config}
+          data={mockData}
+        />
+      </CommonContextMenu>
+    )
+  }
+}
+
+export default ContextMenu
---
+++ src/editor_sorting/store.js
@@ -0,0 +1,110 @@
+import { action } from 'mobx'
+import {
+  SORTING_DETAIL,
+  SORTING_DETAIL_NO_BREAK,
+  SORTING_NO_DETAIL
+} from './constants'
+import EditorStore from '../common/editor_store'
+
+class Store extends EditorStore {
+  constructor({ defaultTableDataKey }) {
+    super()
+    this.defaultTableDataKey = defaultTableDataKey // 修改默认dataKey
+  }
+
+  /* start---------设置采购明细相关--------- */
+  @action.bound
+  setPurchaseTableKey(dataKey) {
+    // 先移除选中项,安全第一
+    this.selected = null
+    this.setTableDataKey(dataKey)
+
+    const arr = this.selectedRegion.split('.')
+    const tableConfig = this.config.contents[arr[2]]
+
+    // 先去掉所有明细列
+    const newCols = tableConfig.columns.filter(
+      o =>
+        !o.isPurchaseDetailByOrderUnit &&
+        !o.isSpecialColumn &&
+        !o.isCustomerColumn
+    )
+    tableConfig.columns.replace(newCols)
+    // 单列-总表最后一列,在columns上修改
+    this.changeSheetUnitSummary(false)
+    if (
+      dataKey === 'purchase_last_col' ||
+      dataKey === 'purchase_last_col_noLineBreak'
+    ) {
+      tableConfig.columns.push(
+        dataKey === 'purchase_last_col'
+          ? SORTING_DETAIL
+          : SORTING_DETAIL_NO_BREAK
+      )
+      dataKey === 'purchase_last_col'
+        ? (tableConfig.columns[
+            tableConfig.columns?.length - 1
+          ].detailLastColType = 'purchase_last_col')
+        : (tableConfig.columns[
+            tableConfig.columns?.length - 1
+          ].detailLastColType = 'purchase_last_col_noLineBreak')
+    } else {
+      tableConfig.columns.push(SORTING_NO_DETAIL)
+    }
+  }
+
+  @action.bound
+  changeSheetUnitSummary(bool) {
+    this.config.isSheetUnitSummary = bool
+  }
+
+  @action.bound
+  setSpecialText(value) {
+    const arr = this.selectedRegion.split('.')
+    const tableConfig = this.config.contents[arr[2]]
+
+    tableConfig.specialConfig.template_text = value
+    // 单列-总表最后一列,在columns上修改
+    if (
+      tableConfig.dataKey === 'purchase_last_col' ||
+      tableConfig.dataKey === 'purchase_last_col_noLineBreak'
+    ) {
+      const specialCol = tableConfig.columns.find(o => o.isSpecialColumn)
+      specialCol.text = value
+    }
+  }
+
+  @action.bound
+  setSpecialStyle(value) {
+    const arr = this.selectedRegion.split('.')
+    const tableConfig = this.config.contents[arr[2]]
+
+    tableConfig.specialConfig.style = value
+    // 单列-总表最后一列,在columns上修改
+    if (
+      tableConfig.dataKey === 'purchase_last_col' ||
+      tableConfig.dataKey === 'purchase_last_col_noLineBreak'
+    ) {
+      const specialCol = tableConfig.columns.find(o => o.isSpecialColumn)
+      specialCol.style = value
+    }
+  }
+
+  @action.bound
+  specialTextAddField(fieldText) {
+    const arr = this.selectedRegion.split('.')
+    const tableConfig = this.config.contents[arr[2]]
+
+    tableConfig.specialConfig.template_text += fieldText
+    // 单列-总表最后一列,在columns上修改
+    if (
+      tableConfig.dataKey === 'purchase_last_col' ||
+      tableConfig.dataKey === 'purchase_last_col_noLineBreak'
+    ) {
+      const specialCol = tableConfig.columns.find(o => o.isSpecialColumn)
+      specialCol.text += fieldText
+    }
+  }
+}
+
+export default new Store({ defaultTableDataKey: 'sorting_detail' })
